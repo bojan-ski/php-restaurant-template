e69a02e9ae0f07f347678e5f409ac4f4b9bea1da
Bojan SKI <codeucenje@gmail.com>
2024-12-01 09:15:39 +0100

    app created

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Hello World from PHP <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Hello World from browser
+    </h1>
+</body>
+</html>
