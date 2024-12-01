13350e41ff5dc38c307da1e739dedd2b49e697cf
Bojan SKI <codeucenje@gmail.com>
2024-12-01 10:38:20 +0100

    home page - reservation section done

--- inc/footer.php
+++ inc/footer.php
@@ -1,67 +1,67 @@
 <footer class="footer" id="footer">
-        <div class="container">
-            <div class="section-header rounded-5">
-                <h2>
-                    Social
-                </h2>
-            </div>
+    <div class="container">
+        <div class="section-header rounded-5">
+            <h2>
+                Social
+            </h2>
+        </div>
 
-            <div class="row mb-4">
-                <!-- row item 1 -->
-                <div class="col-6">
-                    <p class="capitalize text-white">
-                        podaci o firmi
+        <div class="row mb-4">
+            <!-- row item 1 -->
+            <div class="col-6">
+                <p class="capitalize text-white">
+                    podaci o firmi
+                </p>
+
+                <h3 class="capitalize text-white fw-bold mb-5">
+                    San Marco
+                </h3>
+
+                <div class="firm-data">
+                    <p class="text-white mb-0 fw-bold">
+                        UR. SAN MARCO PLUS
                     </p>
 
-                    <h3 class="capitalize text-white fw-bold mb-5">
-                        San Marco
-                    </h3>
+                    <p class="text-white mb-0">
+                        Milene Pavlović Barili 4
+                    </p>
 
-                    <div class="firm-data">
-                        <p class="text-white mb-0 fw-bold">
-                            UR. SAN MARCO PLUS
-                        </p>
-    
-                        <p class="text-white mb-0">
-                            Milene Pavlović Barili 4
-                        </p>
-    
-                        <p class="text-white mb-0">
-                            12000 Požarevac
-                        </p>
-    
-                        <a href="mailto:kontakt@sanmarcopo.com" class="text-white mb-0">
-                            kontakt@sanmarcopo.com
-                        </a>
-                    </div>
-                </div>
+                    <p class="text-white mb-0">
+                        12000 Požarevac
+                    </p>
 
-                <!-- row item 2 -->
-                <div class="col-6 text-end">
-                    <img src="assets/san-marco-logo.avif" alt="logo" class="img-fluid">
+                    <a href="mailto:kontakt@sanmarcopo.com" class="text-white mb-0">
+                        kontakt@sanmarcopo.com
+                    </a>
                 </div>
             </div>
 
-            <div class="application-rights mb-3">
-                <p class="text-white mb-0">
-                    © 2024 San Marco. Politika privatnosti
-                </p>
+            <!-- row item 2 -->
+            <div class="col-6 text-end">
+                <img src="assets/san-marco-logo.avif" alt="logo" class="img-fluid">
             </div>
         </div>
 
-        <div class="social-links text-center">
-            <a href="https://www.instagram.com/san_marco_plus/?hl=en" target="_blank">
-                <img src="assets/instagram-logo.avif" alt="instagram-logo" class="social-link-logo me-3">
-            </a>
-
-            <a href="https://www.facebook.com/sanmarcopozarevac/" target="_blank">
-                <img src="assets/facebook-logo.avif" alt="facebook-logo" class="social-link-logo">
-            </a>
+        <div class="application-rights mb-3">
+            <p class="text-white mb-0">
+                © 2024 San Marco. Politika privatnosti
+            </p>
         </div>
-    </footer>
+    </div>
 
-    <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
+    <div class="social-links text-center">
+        <a href="https://www.instagram.com/san_marco_plus/?hl=en" target="_blank">
+            <img src="assets/instagram-logo.avif" alt="instagram-logo" class="social-link-logo me-3">
+        </a>
+
+        <a href="https://www.facebook.com/sanmarcopozarevac/" target="_blank">
+            <img src="assets/facebook-logo.avif" alt="facebook-logo" class="social-link-logo">
+        </a>
+    </div>
+</footer>
+
+<!-- bootstrap -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <!-- custom css -->
     <link rel="stylesheet" href="css/style.css">
 
-
     <title>San Marco Plus</title>
 </head>
 
@@ -19,10 +18,47 @@
     </header>
 
     <main>
-        <h1 class="text-primary">
-            Hello World from browser
-        </h1>
 
+        <!-- reservations -->
+        <section class="location">
+            <div class="section-header rounded-5">
+                <h2 class="mb-0">
+                    Gde se nalazimo
+                </h2>
+            </div>
+
+            <!-- location data -->
+            <div class="container">
+                
+            </div>
+
+            <!-- location map -->
+        </section>
+
+        <!-- reservations -->
+        <section class="reservations border-bottom mb-3">
+            <div class="container">
+                <div class="section-header rounded-5">
+                    <h2 class="mb-0">
+                        Reservacije
+                    </h2>
+                </div>
+
+                <div class="reservation-option text-center">
+                    <h2 class="capitalize fw-bold text-white">
+                        REZERVIŠI STO
+                    </h2>
+
+                    <p class="text-white">
+                        Za rezervaciju stola pozovite nas.
+                    </p>
+
+                    <a href="tel:+3811220512122" class="phone-num fst-italic fw-bold px-5 py-3 rounded-5">
+                        Kontaktiraj Nas
+                    </a>
+                </div>
+            </div>
+        </section>
     </main>
 
-<?php include 'inc/footer.php' ?>
+    <?php include 'inc/footer.php' ?>
