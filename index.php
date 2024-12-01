99c1d117a6866effd4bc4a599533c128f3a7f423
Bojan SKI <codeucenje@gmail.com>
2024-12-01 09:29:25 +0100

    bootstrap added

--- index.php
+++ index.php
@@ -7,11 +7,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>San Marco Plus</title>
 </head>
 <body>
-    <h1>
+    <h1 class="text-primary">
         Hello World from browser
     </h1>
+
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
