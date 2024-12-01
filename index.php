6c9c85ed0c66d4f396f3d9a92abca8c480fef124
Bojan SKI <codeucenje@gmail.com>
2024-12-01 10:11:17 +0100

    footer component created

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<?php
-    echo "Hello World from PHP <br>";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,12 +12,17 @@
 
     <title>San Marco Plus</title>
 </head>
-<body>
-    <h1 class="text-primary">
-        Hello World from browser
-    </h1>
 
-    <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+<body class="bg-dark">
+    <header>
+
+    </header>
+
+    <main>
+        <h1 class="text-primary">
+            Hello World from browser
+        </h1>
+
+    </main>
+
+<?php include 'inc/footer.php' ?>
